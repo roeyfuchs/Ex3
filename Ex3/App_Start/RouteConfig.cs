e33c2239fc725ce50e963cca81c0a19d469f06a0
Stav Koren <stavkoren123@gmail.com>
2019-05-22 16:38:53 +0300

    update 0.4

--- Ex3/App_Start/RouteConfig.cs
+++ Ex3/App_Start/RouteConfig.cs
@@ -16,14 +16,12 @@ namespace Ex3
             routes.MapRoute("display", "display/{ip}/{port}/{interval}",
                 defaults: new { controller = "Info", action = "display", interval="0"});
 
-            routes.MapRoute("save", "save/{ip}/{port}/{interval}/{samplingTime}/{fileName}",
-               defaults: new { controller = "Info", action = "save" });
             /*routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Info", action = "display", id = UrlParameter.Optional }
             );*/
-
+           
         }
     }
 }
