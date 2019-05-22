11ab99af6b9f7e1253997cf0803d586493708064
Stav Koren <stavkoren123@gmail.com>
2019-05-22 22:17:19 +0300

    create file log mode, flight details

---
+++ Ex3/Models/FlightDetails.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Ex3.Models
+{
+    public class FlightDetails
+    {
+        private const string comma = ",";
+        private const string newLine = "\n";
+        public double LON { set; get; }
+        public double LAT { set; get; }
+        public new string ToString => LON + comma + LAT+newLine;
+    }
+}
---
+++ Ex3/Models/FlightLogModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Web;
+
+namespace Ex3.Models
+{
+    public class FlightLogModel
+    {
+        string fileName;
+        public FlightLogModel(string fileName)
+        {
+            this.fileName = fileName;   
+        }
+        private void PropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            //write data
+            System.IO.File.AppendAllText(fileName, e.ToString());
+        }
+
+    }
+}
