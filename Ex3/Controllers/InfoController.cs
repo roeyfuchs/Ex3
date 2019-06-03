d0ecc08ca3c84c293ac4c1e5d47776e9e6e86dc9
fuchsroey <fuchsroey@gmail.com>
2019-06-03 17:14:20 +0300

    finishe 0.1

--- Ex3/Controllers/InfoController.cs
+++ Ex3/Controllers/InfoController.cs
@@ -8,130 +8,75 @@ using System.Timers;
 using System.Web;
 using System.Web.Mvc;
 
-namespace Ex3.Controllers
-{
+namespace Ex3.Controllers {
     public class InfoController : Controller {
 
         IInfoModel client;
-        private static bool runAnimation;
         private const int miliToSecond = 1000;
         FlightLogModel flightLogModel;
-        private SamplingData samplingData;
-        /// <summary>
-        /// return Index view
-        /// </summary>
-        /// <returns>Action result</returns>
+        private Timer timer;
+
+
+        // GET: Info
         public ActionResult Index() {
             return View();
         }
 
-       /// <summary>
-       /// display- get flight information from the server or from text file
-       /// </summary>
-       /// <param name="ip"></param>
-       /// <param name="port"></param>
-       /// <param name="interval"></param>
-       /// <returns>action result</returns>
         [HttpGet]
         public ActionResult display(string ip, int port, int interval) {
             //display/ip/port/ interval - optional
+            //bbbjjhjfh
             System.Net.IPAddress iPAddress;
             if (System.Net.IPAddress.TryParse(ip, out iPAddress)) {
-                runAnimation = false;
                 ClientModel client = ClientModel.Instance;
                 client.Reset();
                 client.Ip = ip;
                 client.Port = port;
-                client.Interval = (int)((1 / (Double)interval) * 1000);
                 client.Start();
                 this.client = client;
-                //save info on view bag
+
                 this.client.PropertyChanged += this.Client_PropertyChanged;
-               
+
+                ViewBag.Interval = (int)((1 / (Double)interval) * 1000);
                 ViewBag.Lon = Double.NaN;
                 ViewBag.Lat = Double.NaN;
             } else {
                 //display/file name/interval
                 string filePath = ip;
                 int animationTime = port;
-                runAnimation = true;
                 this.flightLogModel = FlightLogModel.Instance;
-                this.flightLogModel.FileName = filePath;
             }
-            ViewBag.Interval = (int)((1 / (Double)interval) * 1000);
-            ViewBag.sec = interval;
             return View();
         }
-        /// <summary>
-        /// save flight information
-        /// </summary>
-        /// <param name="ip"></param>
-        /// <param name="port"></param>
-        /// <param name="interval"></param>
-        /// <param name="samplingTime"></param>
-        /// <param name="fileName"></param>
-        /// <returns>action result</returns>
         [HttpGet]
         public ActionResult save(string ip, int port, int interval, int samplingTime, string fileName) {
             this.flightLogModel = FlightLogModel.Instance;
             this.flightLogModel.FileName = fileName;
-            runAnimation = false;
-            //update sampling object
-            samplingData = new SamplingData(interval * samplingTime);
             ActionResult actionResult = this.display(ip, port, interval);
-            //assign documanting method
             this.client.PropertyChanged += flightLogModel.PropertyChanged;
-            //assign counter
-            flightLogModel.SamplingCounter += this.CountSamplingRaised;
+            this.timer = new Timer();
+            timer.Elapsed += new ElapsedEventHandler(OnTimedEvent);
+            timer.Interval = miliToSecond * samplingTime;
+            timer.Enabled = true;
             return actionResult;
         }
-        /// <summary>
-        /// stop sampling data
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        private void CountSamplingRaised(object sender,EventArgs e)
-        {
-            if (this.samplingData != null)
-            {
-                if (!samplingData.Sample())
-                {
-                    samplingData = null;
-                    client.PropertyChanged -= flightLogModel.PropertyChanged;
-                }
-            }
+        private void OnTimedEvent(object source, ElapsedEventArgs e) {
+            System.Diagnostics.Debug.WriteLine("bye");
+            this.client.PropertyChanged -= flightLogModel.PropertyChanged;
+            this.timer.Enabled = false;
         }
-        /// <summary>
-        /// update view bag with flight data from the server
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
         private void Client_PropertyChanged(object sender, FlightDetailsEventArgs e) {
             ViewBag.Lon = e.FlightDetails.Lon;
             ViewBag.Lat = e.FlightDetails.Lat;
             ViewBag.rudder = e.FlightDetails.Rudder;
             ViewBag.throttle = e.FlightDetails.Throttle;
         }
-        /// <summary>
-        /// return current flight data- from server or text file
-        /// </summary>
-        /// <returns>return xml string</returns>
+
         [HttpPost]
         public string SetValues() {
-            if (runAnimation)
-            {
-                //flight animation
-                FlightLogModel fl = FlightLogModel.Instance;
-                return fl.GetCurrentFlightDetails();
-
-            }
-            else
-            {
-                //get values from the server
-                ClientModel cl = ClientModel.Instance;
-                string str = cl.GetValues();
-                return str;
-            };
+            ClientModel cl = ClientModel.Instance;
+            string str = cl.GetValues();
+            return str;
         }
     }
 }
--- Ex3/Models/ClientModel.cs
+++ Ex3/Models/ClientModel.cs
@@ -1,6 +1,5 @@
 ﻿using Ex3.Models.Interface;
 using System;
-using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
@@ -16,18 +15,13 @@ using System.Xml;
 namespace Ex3.Models {
     public class ClientModel : IInfoModel {
         private Socket socket;
-        private const int bufferSize = 512;
+        private const int bufferSize = 1024;
         private const string lonCommand = "get /position/longitude-deg\r\n";
         private const string latCommand = "get /position/latitude-deg\r\n";
         private const string throttleCommand = "get /controls/engines/current-engine/throttle\r\n";
         private const string rudderCommand = "get /controls/flight/rudder\r\n";
-        private bool online = true;
-
-       
         public event infoHandel PropertyChanged;
         private static ClientModel s_instace = null;
-        private BlockingCollection<FlightDetails> qt = new BlockingCollection<FlightDetails>();
-
 
 
 
@@ -44,46 +38,22 @@ namespace Ex3.Models {
         public int Port { set; get; }
 
         public string GetValues() {
-            StringBuilder sb = new StringBuilder();
-            XmlWriterSettings settings = new XmlWriterSettings();
-            XmlWriter writer = XmlWriter.Create(sb, settings);
-            writer.WriteStartDocument();
-            writer.WriteStartElement("infos");
-            string Lon, Lat, Throttle, Rudder;
-            Lon = Lat = Throttle = Rudder = string.Empty;
-            bool haveElement = true;
-            while (haveElement) {
-                System.Diagnostics.Debug.WriteLine(this.qt.Count);
-                FlightDetails fd;
-                fd = this.qt.Take();
-                fd.AddToString(ref Lon, ref Lat, ref Throttle, ref Rudder);
-                if(this.qt.Count == 0) {
-                    haveElement = false;
-                }
-            }
-            writer.WriteStartElement("info");
-            writer.WriteElementString("Lon", Lon);
-            writer.WriteElementString("Lat", Lat);
-            writer.WriteElementString("Throttle", Throttle);
-            writer.WriteElementString("Rudder", Rudder);
-            writer.WriteEndElement();
-            writer.WriteEndElement();
-            writer.WriteEndDocument();
-            writer.Flush();
-            return sb.ToString();
-        }
-
-        public int Interval { get; set; }
-
-
-        public void RefreshData() {
             double lat = GetLat();
             double lon = GetLon();
             double throttle = GetThrottle();
             double rudder = GetRudder();
             FlightDetails flightDetails = new FlightDetails(lon, lat, throttle, rudder);
             this.PropertyChanged?.Invoke(this, new FlightDetailsEventArgs(flightDetails));
-            this.qt.Add(flightDetails);
+            StringBuilder sb = new StringBuilder();
+            XmlWriterSettings settings = new XmlWriterSettings();
+            XmlWriter writer = XmlWriter.Create(sb, settings);
+            writer.WriteStartDocument();
+            writer.WriteStartElement("infos");
+            flightDetails.ToXml(writer);
+            writer.WriteEndElement();
+            writer.WriteEndDocument();
+            writer.Flush();
+            return sb.ToString();
         }
 
         private double GetLat() {
@@ -92,6 +62,7 @@ namespace Ex3.Models {
         private double GetLon() {
             return GetInfo(lonCommand);
         }
+
         private double GetThrottle() {
             return GetInfo(throttleCommand);
         }
@@ -100,42 +71,28 @@ namespace Ex3.Models {
         }
 
         private double GetInfo(string str) {
-            System.Diagnostics.Debug.WriteLine("Here");
-            this.socket.NoDelay = true;
             this.socket.Send(System.Text.Encoding.ASCII.GetBytes(str));
             byte[] buffer = new byte[bufferSize];
             int iRx = socket.Receive(buffer);
             string recv = Encoding.ASCII.GetString(buffer, 0, iRx);
-            System.Diagnostics.Debug.WriteLine("Done");
+            recv = recv.Split('\r')[0];
             return FromSimToDobule(recv);
         }
 
         public double Lat { get; set; }
         public double Lon { get; set; }
 
-       
 
-        public void Start() { 
-            Task t = new Task(() => {
-                this.ClientRunning();
-            });
-            t.Start();
-        }
 
-        private void ClientRunning() {
+        public void Start() {
             this.socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
             IPAddress ipAdd = IPAddress.Parse(this.Ip);
             IPEndPoint remoteEP = new IPEndPoint(ipAdd, this.Port);
             this.socket.Connect(remoteEP);
-            while (this.online) {
-                this.RefreshData();
-                Thread.Sleep(this.Interval);
-            }
-            this.socket.Close();
         }
 
         public void Stop() {
-            this.online = false;
+            this.socket.Close();
         }
 
         public void Reset() {
